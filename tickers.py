ca19fcf4040b429d69be941e37246f455e9a2a30
salomon-alt <qugecip254@gmail.com>
2026-06-23 17:41:13 +0400

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -1,10 +1,19 @@
-# tickers.py
 TICKER_GROUPS = {
-    #"акции": ["SBER", "GAZP", "ROSN", "NVTK", "SPBE", "SGZH", "SMLT", "PIKK", "SNGS", "FESH", "SIBN", "MTLRP", "SOFL", "RASP", "RUAL", "VKCO", "POSI", "YDEX"],
-    "валюты": ["USD000UTSTOM", "CNYRUB_TOM", "GLDRUB_TOM", "SLVRUB_TOM"],
-   # "облигации": ["SU26233RMFS5", "SU26238RMFS4", "SU26240RMFS0", "SU26248RMFS3"],
+    "валюты": [
+        "USD000UTSTOM",
+        "CNYRUB_TOM"
+    ]
 }
 
-def get_timeframes_for_ticker(ticker: str) -> list:
-    # Возвращаем список таймфреймов для тикера
+
+def get_timeframes_for_ticker(ticker: str):
+
+    if ticker in [
+        "USD000UTSTOM",
+        "CNYRUB_TOM",
+        "GLDRUB_TOM",
+        "SLVRUB_TOM"
+    ]:
+        return ["day", "1h"]
+
     return ["week", "day", "4h", "1h"]
